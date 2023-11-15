34fe4d5e7dba891a82eb31ecab93220445dc60a6
ter1203 <terry.hopper1203@gmail.com>
2023-11-14 20:33:57 -0500

    track prev color palette

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React from "react";
 function App() {
   const [colors, setColors] = React.useState([
-    '#FFD500',
-    '#FF0040',
+    "#FFD500",
+    "#FF0040",
+    "#FF0040",
+    "#FF0040",
+    "#FF0040",
   ]);
-  
-  const colorStops = colors.join(', ');
+  const [numOfVisibleColors, setNumOfVisibleColors] = React.useState(2);
+
+  const visibleColors = colors.slice(0, numOfVisibleColors);
+  const colorStops = colors.join(", ");
   const backgroundImage = `linear-gradient(${colorStops})`;
+
+  function addColor() {
+    if (numOfVisibleColors.length >= 5) return;
+
+    setNumOfVisibleColors(numOfVisibleColors + 1);
+  }
+
+  function removeColor() {
+    if (numOfVisibleColors.length <= 2) return;
+
+    setNumOfVisibleColors(numOfVisibleColors - 1);
+  }
 
   return (
     <div className="wrapper">
       <div className="actions">
-        <button>
-          Remove color
-        </button>
-        <button>
-          Add color
-        </button>
+        <button onClick={removeColor}>Remove color</button>
+        <button onClick={addColor}>Add color</button>
       </div>
-      
+
       <div
         className="gradient-preview"
         style={{
           backgroundImage,
         }}
       />
-      
+
       <div className="colors">
-        {colors.map((color, index) => {
+        {visibleColors.map((color, index) => {
           const colorId = `color-${index}`;
           return (
             <div key={colorId} className="color-wrapper">
-              <label htmlFor={colorId}>
-                Color {index + 1}:
-              </label>
+              <label htmlFor={colorId}>Color {index + 1}:</label>
               <div className="input-wrapper">
                 <input
                   id={colorId}
                   type="color"
+                  value={color}
+                  onChange={(event) => {
+                    const newColors = [...colors];
+                    newColors[index] = event.target.value;
+                    setColors(newColors);
+                  }}
                 />
               </div>
             </div>
